487bb7121c4767a472e95e7e60ed1c1c2ff59e43
Jeremy Magland <jmagland@flatironinstitute.org>
2022-10-26 18:58:21 -0400

    gh action

--- actions/process-log-items/src/index.ts
+++ actions/process-log-items/src/index.ts
@@ -1,6 +1,7 @@
 // import { DocumentSnapshot } from '@google-cloud/firestore'
 
 const processLogItems = async () => {
+    console.info(`TESTVAR: ${process.env["TESTVAR"]}`)
     console.info('process-log-items:::')
 }
 
